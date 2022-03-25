3f8e9ff5ed4a37245a772900c54916758c0bbf14
jsg <jasonisgraham@gmail.com>
2022-03-25 17:08:40 -0400

    adapted planck's conf to preonic

--- keyboards/preonic/keymaps/jasonisgraham/config.h
+++ keyboards/preonic/keymaps/jasonisgraham/config.h
@@ -1,23 +1,25 @@
-/* Copyright 2015-2021 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
+#include "song_list.h"
+
+
+#define LEADER_TIMEOUT 300
+
+#ifdef AUDIO_ENABLE
+#    define STARTUP_SONG SONG(PREONIC_SOUND)
+// #define STARTUP_SONG SONG(NO_SOUND)
+
+#    define DEFAULT_LAYER_SONGS                                   \
+  { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
+#endif
+
+#define MUSIC_MASK (keycode != KC_NO)
+
+#define FORCE_NKRO
+
+
 #define AUTO_SHIFT_TIMEOUT 150
-#define NO_AUTO_SHIFT_SPECIAL
+/* #define AUTO_SHIFT_SPECIAL */
 
 /*
   Set any config.h overrides for your specific keymap here.
@@ -44,22 +46,13 @@
 #define RGB_MATRIX_STARTUP_SPD 60
 
 
-#ifdef AUDIO_ENABLE
-#    define STARTUP_SONG SONG(PREONIC_SOUND)
-// #define STARTUP_SONG SONG(NO_SOUND)
-
-#    define DEFAULT_LAYER_SONGS \
-        { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
-#endif
-
-#define MUSIC_MASK (keycode != KC_NO)
 
 /*
  * MIDI options
  */
 
 /* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
+ #@109  - MIDI notes can be sent when in Music mode is on
 */
 
 #define MIDI_BASIC
@@ -70,7 +63,11 @@
    - Virtual sustain, portamento, and modulation wheel
    - etc.
 */
-//#define MIDI_ADVANCED
+#define MIDI_ADVANCED
 
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+#ifndef WEBUSB_ENABLE
+#    define WEBUSB_PAIR KC_NO
+#endif
