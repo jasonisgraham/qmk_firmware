f582b086847560f2f3bd584749442fc047cd8366
jsg <jasonisgraham@gmail.com>
2022-03-25 17:08:40 -0400

    adapted planck's conf to preonic

--- keyboards/preonic/keymaps/jasonisgraham/rules.mk
+++ keyboards/preonic/keymaps/jasonisgraham/rules.mk
@@ -1,10 +1,19 @@
 SRC += muse.c
+
+LEADER_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
+# COMMAND_ENABLE = no
 AUTO_SHIFT_ENABLE = yes
 WEBUSB_ENABLE = yes
-ORYX_ENABLE = yes
+# ORYX_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
-TAP_DANCE_ENABLE = yes
+# BACKLIGHT_ENABLE = yes
+# RGBLIGHT_ENABLE = yes
+
+AUDIO_ENABLE = yes
+
+NKRO_ENABLE = yes
