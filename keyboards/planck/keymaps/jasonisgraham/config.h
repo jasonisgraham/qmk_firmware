a99f7f6acc2ea364b7b9e183bfdfaf4ecfd8923a
jsg <jasonisgraham@gmail.com>
2022-03-29 15:50:09 -0400

    importing planck config from oryx

---
+++ keyboards/planck/keymaps/jasonisgraham/config.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "config_common.h"
+
+#define COMBO_COUNT 50
+#define COMBO_TERM 20  // default is 50.  50ms produces false positives when typing kinda fast
+
+
+#define FORCE_NKRO
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(PLANCK_SOUND)
+#endif
+
+#define MIDI_BASIC
+
+#define ENCODER_RESOLUTION 4
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef TAPPING_TERM
+#define TAPPING_TERM 195
+
+#define PERMISSIVE_HOLD
+
+#undef IGNORE_MOD_TAP_INTERRUPT
+
+#undef RGB_MATRIX_VAL_STEP
+#define RGB_MATRIX_VAL_STEP 11
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 16
+
+#define NO_AUTO_SHIFT_TAB
+#define HOLD_ON_OTHER_KEY_PRESS
+#define FIRMWARE_VERSION u8"jaa0Y/bQZQp"
+#define RGB_MATRIX_STARTUP_SPD 60
--- planck/planck_ez_glow_jasonisgraham-taps-are-back-baby_source/rules.mk
+++ planck/planck_ez_glow_jasonisgraham-taps-are-back-baby_source/rules.mk
@@ -1,13 +1,10 @@
+SRC += muse.c
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-LEADER_ENABLE = yes
-COMBO_ENABLE = yes
-NKRO_ENABLE = yes
 CONSOLE_ENABLE = no
-# COMMAND_ENABLE = no
+COMMAND_ENABLE = no
 AUTO_SHIFT_ENABLE = yes
 WEBUSB_ENABLE = yes
 ORYX_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
 TAP_DANCE_ENABLE = yes
-SRC = matrix.c
