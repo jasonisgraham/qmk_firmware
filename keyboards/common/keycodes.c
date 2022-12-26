606b9fc1103d65f7c24234d8f5837a28effcd3ab
jsg <jasonisgraham@gmail.com>
2022-12-26 18:34:34 -0500

    nothing real big.  evil-jump-item i guess. plus tons of other dumb shit

--- keyboards/common/keycodes.c
+++ keyboards/common/keycodes.c
@@ -7,14 +7,25 @@
 /* super_meta_hyper */
 #define GUI LGUI
 
+#define key_0_7 KC_GRAVE
+#define key_1_7 top_alpha_mid_column
+#define key_2_7 backspace
+#define key_3_7 KC_ENTER
+/* #define key_4_7 TD(dance_k74) */
 
-#define SET_ADHOC_0_1 LCTL(LALT(LGUI(KC_F1)))
-#define SET_ADHOC_0_4 LCTL(LALT(LGUI(KC_F4)))
-#define SET_ADHOC_0_6 LCTL(LALT(LGUI(KC_F6)))
+#define SET_HOTKEY_1 LCTL(LALT(LGUI(KC_F1)))
+#define SET_HOTKEY_2 LCTL(LALT(LGUI(KC_F2)))
+#define SET_HOTKEY_4 LCTL(LALT(LGUI(KC_F4)))
+#define SET_HOTKEY_5 LCTL(LALT(LGUI(KC_F5)))
+#define SET_HOTKEY_6 LCTL(LALT(LGUI(KC_F6)))
+#define SET_HOTKEY_0 LCTL(LALT(LGUI(KC_F12)))
 
-#define SELECT_ADHOC_0_1 LALT(LGUI(KC_F1))
-#define SELECT_ADHOC_0_4 LALT(LGUI(KC_F4))
-#define SELECT_ADHOC_0_6 LALT(LGUI(KC_F6))
+#define SELECT_HOTKEY_1 LALT(LGUI(KC_F1))
+#define SELECT_HOTKEY_2 LALT(LGUI(KC_F2))
+#define SELECT_HOTKEY_4 LALT(LGUI(KC_F4))
+#define SELECT_HOTKEY_5 LALT(LGUI(KC_F5))
+#define SELECT_HOTKEY_6 LALT(LGUI(KC_F6))
+#define SELECT_HOTKEY_0 LALT(LGUI(KC_F12))
 
 
 #define SELECT_TO_LINE_HOME LSFT(KC_HOME)
@@ -46,19 +57,28 @@
 #define WINDOW_ALWAYS_ON_TOP LCTL(LSFT(LGUI(RCTL(KC_UP))))
 #define WINDOW_TOGGLE_HORIZONTAL_MAX LSFT(LGUI(RCTL(KC_MINUS)))
 
-#define winmove_mon_up LGUI(LSFT(KC_UP))
-#define winmove_mon_left LGUI(LSFT(KC_LEFT))
-#define winmove_mon_right LGUI(LSFT(KC_RIGHT))
-#define winmove_mon_down LGUI(LSFT(KC_DOWN))
+#define winmove_U_monitor LGUI(LSFT(KC_UP))
+#define winmove_L_monitor LGUI(LSFT(KC_LEFT))
+#define winmove_R_monitor LGUI(LSFT(KC_RIGHT))
+#define winmove_D_monitor LGUI(LSFT(KC_DOWN))
 
-#define winmove_tile_up LGUI(LSFT(KC_K))
-#define winmove_tile_left LGUI(LSFT(KC_H))
-#define winmove_tile_right LGUI(LSFT(KC_L))
-#define winmove_tile_down LGUI(LSFT(KC_J))
+#define winmove_U LGUI(LSFT(KC_K))
+#define winmove_D LGUI(LSFT(KC_J))
+
+#define winmove_UL LGUI(LSFT(KC_Y))
+#define winmove_UR LGUI(LSFT(KC_O))
+#define winmove_R LGUI(LSFT(KC_L))
+#define winmove_L LGUI(LSFT(KC_H))
+#define winmove_DL LGUI(LSFT(KC_N))
+#define winmove_DR LGUI(LSFT(KC_DOT))
+
 
 #define editing_j LALT(LSFT(KC_J))
 #define editing_k LALT(LSFT(KC_K))
 #define editing_l LALT(LSFT(KC_L))
+#define next_win_or_frame LALT(RCTL(KC_N))
+#define prev_win_or_frame LALT(RCTL(KC_P))
+
 /* #define EMACS_LOCCUR RCTL(LALT(LGUI(KC_O))) */
 #define CTRL_L RCTL(KC_P)
 #define CTRL_N RCTL(KC_N)
@@ -182,12 +202,10 @@
 #define my_semicolon KC_SCOLON
 #define my_singlequote KC_QUOTE
 #define my_v KC_V
-#define next_win_or_frame LALT(LSFT(KC_N))
-#define prev_win_or_frame LALT(LSFT(KC_P))
 #define raise_space KC_UNDS
 #define raise_key_4_9 KC_ENTER // KC_BSPC
 #define raise_semi  KC_MINUS
-#define raise_slash KC_BSLASH
+#define raise_slash KC_UNDS
 /* #define right_of_lower  esc_ctrl  // my_left_shift ///hyper // / MT(MOD_RCTL, KC_ESCAPE) */
 #define lower_right_of_super TD(WWW_BACK_FORWARD)
 #define scroll_next RCTL(KC_D)
@@ -206,9 +224,9 @@
 #define EMACS_SCROLL_UP RCTL(KC_Y)
 
 
-#define toggle_microphone RCTL(LALT(LGUI(LCTL(KC_F4))))
-#define on_microphone RCTL(LALT(LGUI(LCTL(KC_F5))))
-#define off_microphone RCTL(LALT(LGUI(LCTL(KC_F6))))
+#define toggle_microphone LALT(LGUI(KC_F7))
+#define on_microphone LALT(LGUI(KC_F8))
+#define off_microphone LALT(LGUI(KC_F9))
 
 #define top_alpha_mid_column TD(DANCE_MICROPHONE)
 
@@ -280,6 +298,7 @@ enum custom_keycodes {
                       /* RGB_SLD = SAFE_RANGE, */
                       /* RGB_SLD = EZ_SAFE_RANGE, */
                       FIRST = SAFE_RANGE,
+                      TMPTXT,
                       ALT,
                       TO_BASE,
                       LLOCK,
@@ -350,7 +369,7 @@ enum custom_keycodes {
                       EMACS_GOTO_ROLLUP,
                       TOGGLE_ECHO,
                       TOGGLE_BREATHING,
-                      EMACS_ACE_WINDOW,
+                      EMACS_ACE_WINDOW_SELECT,
                       TERM_HOME,
                       CD_CSV,
                       EMACS_SEL_0,
@@ -720,3 +739,4 @@ bool do_breathing = false;
 /* SS_LGUI(SS_TAP(X_E)) SS_DELAY(100) SS_LGUI(SS_TAP(X_E)) SS_DELAY(100) SS_LALT(SS_TAP(X_M)) */
 
 #define right_of_super MO(_APL)  // X(BANG) // esc_ctrl //  BRACKET_PAREN //all_mods
+
