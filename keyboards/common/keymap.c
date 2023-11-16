9e186288bea026a0f6e424991c141cd970c2d6dd
jsg <jasonisgraham@gmail.com>
2023-11-15 19:11:20 -0500

    preonic using workman

--- keyboards/common/keymap.c
+++
@@ -1,9 +0,0 @@
-#include QMK_KEYBOARD_H
-#include "version.h"
-#include "muse.h"
-
-/* #include "layers.c" */
-#include "keycodes.c"
-/* #include "tapdance.c" */
-/* #include "combos.c" */
-/* #include "process_record_user.c" */
